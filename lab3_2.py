f69cbf99e009e0e4fc3b36e01ecdef5cccfac02a
LiudaShevliuk <31858018+LiudaShevliuk@users.noreply.github.com>
2018-09-10 12:29:59 +0300

    Add files via upload

---
+++ lab3_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('Enter the temperature in Fahrenheit: ')
+temper_celsius = (int(input()) - 32) * 5 / 9.0
+print('Temperature in Celsius: ', temper_celsius)
