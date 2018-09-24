b4f7ab455df5bd4341b9f9e382274e2d2a286244
LiudaShevliuk <31858018+LiudaShevliuk@users.noreply.github.com>
2018-09-24 12:01:04 +0300

    Add files via upload

---
+++ lab5_3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import random
+
+player = int(input('Choose: 1-rock, 2-scissors, 3-paper: '))
+
+if player > 3 or player < 1:
+    print('Your choice is wrong')
+    exit()
+
+comp = random.randint(1,3)
+
+print('Your choice - ', player, ', Computer\'s  choice - ', comp) 
+
+if player == comp:
+    print('Draw')
+elif player == 1 and comp == 3:
+    print('Computer win')
+elif player == 2 and comp == 1:
+    print('Computer win')
+elif player == 3 and comp == 2:
+    print('Computer win')
+else:
+    print('Player win')
