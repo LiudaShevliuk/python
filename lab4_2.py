395fec457dd80d22e9ef08d42040611e0d8f7b7e
LiudaShevliuk <31858018+LiudaShevliuk@users.noreply.github.com>
2018-09-30 14:58:51 +0300

    Add files via upload

---
+++ lab4_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import math
+
+print('Enter three numbers: ')
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+function = math.exp(-(a-b)**2/(2*c**2)) / (c*math.sqrt(2*math.pi))
+
+print('Result: ', function)
+
