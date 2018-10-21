1236e0e930d626c096a103d660ac86a05bec000e
LiudaShevliuk <31858018+LiudaShevliuk@users.noreply.github.com>
2018-10-21 20:31:42 +0300

    Update lab7_2.py

--- lab7_2.py
+++ lab7_2.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-phrase = input('Enter your phrase: ')
+def ui_input() -> str:
+    """This function takes string for checking"""
+    return input('Enter your phrase: ')
 
-def isPalindrome(string):
+def is_palindrome(string: str) -> bool:
+    """This function does all letters lowercase,\ 
+       deletes all other symbols and checks if it\
+       is a palindrome"""
     string = string.lower()
     symbols = [' ', '\'', '\"', '.', ',', ';', ':', '\\']
     res_string = []
     for _ in string:
         if _ not in symbols:
-            res_string.append(_)
+            res_string.append(_)  
     return res_string == res_string[::-1]
 
-print(isPalindrome(phrase))
+def ui_output(is_pal: bool) -> None:
+    """This function prints the result of cheking"""
+    print(is_pal)
+
+ui_output(is_palindrome(ui_input()))
